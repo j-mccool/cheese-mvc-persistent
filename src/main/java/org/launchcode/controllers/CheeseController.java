8aa80858c5d1b90608d70e9a7a080b3eba779949
John McCool <john.mccool@gmail.com>
2018-06-13 11:52:37 -0500

    part 2 complete, bonus mission not completed for part 2

--- src/main/java/org/launchcode/controllers/CheeseController.java
+++ src/main/java/org/launchcode/controllers/CheeseController.java
@@ -1,7 +1,8 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Category;
 import org.launchcode.models.Cheese;
-import org.launchcode.models.CheeseType;
+import org.launchcode.models.data.CategoryDao;
 import org.launchcode.models.data.CheeseDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,9 @@ public class CheeseController {
     @Autowired
     private CheeseDao cheeseDao;
 
+    @Autowired
+    private CategoryDao categoryDao;
+
     // Request path: /cheese
     @RequestMapping(value = "")
     public String index(Model model) {
@@ -34,23 +38,33 @@ public class CheeseController {
         return "cheese/index";
     }
 
+    //this is a bonus mission for part 2... the issue is the cheeseDao findby; needs work
+    @RequestMapping(value = "category/{categoryId}")
+    public String category(Model model, @RequestParam int categoryId) {
+        model.addAttribute("title", "Cheese by Category");
+        //model.addAttribute("cheeses", cheeseDao.findByOne(categoryId));
+
+        return "cheese/index";
+    }
+
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String displayAddCheeseForm(Model model) {
         model.addAttribute("title", "Add Cheese");
         model.addAttribute(new Cheese());
-        model.addAttribute("cheeseTypes", CheeseType.values());
+        model.addAttribute("categories", categoryDao.findAll());
         return "cheese/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@ModelAttribute  @Valid Cheese newCheese,
-                                       Errors errors, Model model) {
+    public String processAddCheeseForm(@RequestParam int categoryId, Model model, @ModelAttribute  @Valid Cheese newCheese,
+                                       Errors errors) {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Cheese");
             return "cheese/add";
         }
-
+        Category cat = categoryDao.findOne(categoryId);
+        newCheese.setCategory(cat);
         cheeseDao.save(newCheese);
         return "redirect:";
     }
